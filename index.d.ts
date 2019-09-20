9b4f5391f100562f8f72275843ec8caa60cb06a9
yuki akiyama <you2197901@gmail.com>
2019-09-20 19:45:13 +0900

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 export declare const encode: (buffer: Buffer) => string;
 export declare const decodeUnsafe: (string: string) => Buffer | undefined;
 export declare const decode: (string: string) => Buffer;
