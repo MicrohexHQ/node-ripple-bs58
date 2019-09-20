eba84346581c3381ea22d3974a83da53059a4f8a
yuki akiyama <you2197901@gmail.com>
2019-09-20 19:40:10 +0900

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export declare const encode: (buffer: Buffer) => string;
+export declare const decodeUnsafe: (string: string) => Buffer | undefined;
+export declare const decode: (string: string) => Buffer;
